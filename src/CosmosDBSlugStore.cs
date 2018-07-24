4d9b1542acfe76a6dc6aa8da064232d7809cdab9
Alex Brown <alex@cohoda.com>
2018-07-24 09:06:43 +0100

    initial commit - project setup, refs added etc..

---
+++ src/CosmosDBSlugStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SlugHub.SlugStore;
+
+namespace SlugHub.CosmosDB
+{
+    public class CosmosDbSlugStore : ISlugStore
+    {
+        private string _keyPrefix;
+
+        public CosmosDbSlugStore(string keyPrefix)
+        {
+            _keyPrefix = keyPrefix;
+        }
+
+        public bool Exists(string slug, string groupingKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store(Slug slug)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
